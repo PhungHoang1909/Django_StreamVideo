6dd5801f34827a1df453d4028a2a0cd2c497302d
phung hoang <lnphunghoang190903@gmail.com>
2024-05-20 21:45:02 +0700

    Add Consumers and Routing

--- WebRTC/streamproject/settings.py
+++ WebRTC/streamproject/settings.py
@@ -128,3 +128,10 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 ASGI_APPLICATION = "streamproject.asgi.application"
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}
+
