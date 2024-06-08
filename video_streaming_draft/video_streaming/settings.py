e6f79dd87857591fb9f5e8c7d038d01965fa8bea
phung hoang <lnphunghoang190903@gmail.com>
2024-06-08 20:38:59 +0700

    Update FFMPEG

--- video_streaming_draft/video_streaming/settings.py
+++ video_streaming_draft/video_streaming/settings.py
@@ -101,7 +101,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'video_app', 'static', 'streams'),  # Adjusted path
 ]
 
@@ -141,7 +140,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
